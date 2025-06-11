094bc8ce21dd04072615d72ab881437ecb755d97
Hrishabh <hrishabhgautam480@gmail.com>
2025-06-11 10:36:00 +0530

    Added Login and Register Request Dto files(Code)

--- trackify-backend/src/main/java/com/trackify/dto/request/LoginRequest.java
+++ trackify-backend/src/main/java/com/trackify/dto/request/LoginRequest.java
@@ -1,5 +1,53 @@
 package com.trackify.dto.request;
 
-public class LoginRequest {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequest {
+    
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    @Size(max = 255, message = "Email must not exceed 255 characters")
+    private String email;
+    
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 100, message = "Password must be between 6 and 100 characters")
+    private String password;
+    
+    @Builder.Default
+    private Boolean rememberMe = false;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Boolean getRememberMe() {
+		return rememberMe;
+	}
+
+	public void setRememberMe(Boolean rememberMe) {
+		this.rememberMe = rememberMe;
+	}
+    
 }
--- trackify-backend/src/main/java/com/trackify/dto/request/RegisterRequest.java
+++ trackify-backend/src/main/java/com/trackify/dto/request/RegisterRequest.java
@@ -1,5 +1,121 @@
 package com.trackify.dto.request;
 
-public class RegisterRequest {
+import com.trackify.enums.UserRole;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+    
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    @Size(max = 255, message = "Email must not exceed 255 characters")
+    private String email;
+    
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 100, message = "Password must be between 6 and 100 characters")
+    @Pattern(
+        regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&].*$",
+        message = "Password must contain at least one uppercase letter, one lowercase letter, one number and one special character"
+    )
+    private String password;
+    
+    @NotBlank(message = "Confirm password is required")
+    private String confirmPassword;
+    
+    @NotBlank(message = "First name is required")
+    @Size(min = 2, max = 100, message = "First name must be between 2 and 100 characters")
+    private String firstName;
+    
+    @NotBlank(message = "Last name is required")
+    @Size(min = 2, max = 100, message = "Last name must be between 2 and 100 characters")
+    private String lastName;
+    
+    @Pattern(
+        regexp = "^[+]?[1-9]\\d{1,14}$",
+        message = "Phone number should be valid"
+    )
+    private String phoneNumber;
+    
+    @Builder.Default
+    private UserRole role = UserRole.USER;
+    
+    @Builder.Default
+    private Boolean acceptTerms = false;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public UserRole getRole() {
+		return role;
+	}
+
+	public void setRole(UserRole role) {
+		this.role = role;
+	}
+
+	public Boolean getAcceptTerms() {
+		return acceptTerms;
+	}
+
+	public void setAcceptTerms(Boolean acceptTerms) {
+		this.acceptTerms = acceptTerms;
+	}
+    
+    
+    
 }
