14a4998d6247fc41b949374af723c29b1d94a01d
Hrishabh <hrishabhgautam480@gmail.com>
2025-06-11 10:44:06 +0530

    Added SwaggerConfig file (Code)

--- trackify-backend/src/main/java/com/trackify/config/SwaggerConfig.java
+++ trackify-backend/src/main/java/com/trackify/config/SwaggerConfig.java
@@ -1,5 +1,39 @@
 package com.trackify.config;
 
-public class SwaggerConfig {
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+public class SwaggerConfig {
+    
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Trackify API")
+                        .description("Real-time Expense Tracker REST API Documentation")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Trackify Team")
+                                .email("support@trackify.com")
+                                .url("https://github.com/hrishabh-16/Trackify"))
+                        .license(new License()
+                                .name("MIT License")
+                                .url("https://opensource.org/licenses/MIT")))
+                .addSecurityItem(new SecurityRequirement().addList("JWT"))
+                .components(new Components()
+                        .addSecuritySchemes("JWT", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .in(SecurityScheme.In.HEADER)
+                                .name("Authorization")));
+    }
 }
