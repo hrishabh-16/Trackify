af5a84c590eefeb5088f2ad03de946e2cbac6575
Hrishabh <hrishabhgautam480@gmail.com>
2025-06-11 10:45:42 +0530

    Added Exception File : ExpenseTrackerException (Code)

--- trackify-backend/src/main/java/com/trackify/exception/ExpenseTrackerException.java
+++ trackify-backend/src/main/java/com/trackify/exception/ExpenseTrackerException.java
@@ -1,5 +1,47 @@
 package com.trackify.exception;
 
-public class ExpenseTrackerException {
-
+public class ExpenseTrackerException extends RuntimeException {
+    
+    private String errorCode;
+    private Object data;
+    
+    public ExpenseTrackerException(String message) {
+        super(message);
+    }
+    
+    public ExpenseTrackerException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    
+    public ExpenseTrackerException(String message, String errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+    
+    public ExpenseTrackerException(String message, String errorCode, Object data) {
+        super(message);
+        this.errorCode = errorCode;
+        this.data = data;
+    }
+    
+    public ExpenseTrackerException(String message, String errorCode, Throwable cause) {
+        super(message, cause);
+        this.errorCode = errorCode;
+    }
+    
+    public String getErrorCode() {
+        return errorCode;
+    }
+    
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
+    
+    public Object getData() {
+        return data;
+    }
+    
+    public void setData(Object data) {
+        this.data = data;
+    }
 }
