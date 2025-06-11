50efe871daf860bb036cf2e16f2d11e110c3b240
Hrishabh <hrishabhgautam480@gmail.com>
2025-06-11 10:40:04 +0530

    Added CustomUserDetailsService file (Code)

--- trackify-backend/src/main/java/com/trackify/security/CustomUserDetailsService.java
+++ trackify-backend/src/main/java/com/trackify/security/CustomUserDetailsService.java
@@ -1,5 +1,37 @@
 package com.trackify.security;
 
-public class CustomUserDetailsService {
+import com.trackify.entity.User;
+import com.trackify.exception.ResourceNotFoundException;
+import com.trackify.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private  UserRepository userRepository;
+    
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+        
+        return UserPrincipal.create(user);
+    }
+    
+    @Transactional
+    public UserDetails loadUserById(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found with id: " + id));
+        
+        return UserPrincipal.create(user);
+    }
 }
