49e7399f18ff97498a70a0ff3432e2d521fc53cc
Hrishabh <hrishabhgautam480@gmail.com>
2025-06-23 16:43:06 +0530

    Added Budget Components

---
+++ trackify-frontend/src/app/features/budgets/components/budget-progress/budget-progress.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-budget-progress',
+  imports: [],
+  templateUrl: './budget-progress.component.html',
+  styleUrl: './budget-progress.component.css'
+})
+export class BudgetProgressComponent {
+
+}
