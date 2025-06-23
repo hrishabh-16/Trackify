d8770bf8cda4a1320ee96ad1901129bd5522d743
Hrishabh <hrishabhgautam480@gmail.com>
2025-06-23 16:42:39 +0530

    Added Team Components

---
+++ trackify-frontend/src/app/features/teams/components/invite-member/invite-member.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-invite-member',
+  imports: [],
+  templateUrl: './invite-member.component.html',
+  styleUrl: './invite-member.component.css'
+})
+export class InviteMemberComponent {
+
+}
---
+++ trackify-frontend/src/app/features/teams/components/team-detail/team-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-team-detail',
+  imports: [],
+  templateUrl: './team-detail.component.html',
+  styleUrl: './team-detail.component.css'
+})
+export class TeamDetailComponent {
+
+}
