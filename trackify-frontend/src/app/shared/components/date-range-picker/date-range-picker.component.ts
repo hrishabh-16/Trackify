dc944b39b5ce23d32193d5b9e06de011f96f4a14
Hrishabh <hrishabhgautam480@gmail.com>
2025-06-23 16:38:34 +0530

    Added Shared Components

---
+++ trackify-frontend/src/app/shared/components/date-range-picker/date-range-picker.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-date-range-picker',
+  imports: [],
+  templateUrl: './date-range-picker.component.html',
+  styleUrl: './date-range-picker.component.css'
+})
+export class DateRangePickerComponent {
+
+}
